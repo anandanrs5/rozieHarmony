630cc2e75e25a1e7ee7b6bc967f461f337f83ee5
anandanrs5 <anandansmart50@gmail.com>
2023-01-02 22:05:03 +0530

    retrieve information

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,24 +1,20 @@
 import { Injectable } from '@angular/core';
 import { CustomerProfile } from './model/customerProfile';
-
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
-
-  constructor() { }
-
+constructor() { }
   profileData:CustomerProfile={
     profile:
-    {title   :'', lastname:'',language:'',firstname:'',
-    middlename:'',Gender:'',dob:'',ivr_pin_status:'',email:''},
+        { title:'',     lastname:'',language:'',firstname:'',
+          middlename:'',Gender:'',  dob:'',     ivr_pin_status:'',email:''},
     phone:
-    {m_countrycode: '', h_countrycode:'', o_countrycode:'', mobilenumber: '', 
-      homenumber:'', officenumber: '', },
-      address:
-      {choose:'',address1:''  ,address2:''  ,
-        state:   ''  ,zipcode: ''  ,country: ''  ,
-        company: ''  ,city:    ''  }
+        { m_countrycode:'',h_countrycode:'',o_countrycode:'',mobilenumber:'', 
+          homenumber:'',   officenumber:''},
+    address:
+        { choose:'',   address1:'', address2:'',state:'',zipcode:'',
+          country:'',  company:''  ,city:''}
     }
 
   saveDataInServive(customerInformation:any){
